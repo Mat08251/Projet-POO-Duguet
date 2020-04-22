9bf7d0588e921daae79e78c26d4a075207ec0dda
Mat08251 <duguet@simplon-charleville.fr>
2020-04-22 11:37:31 +0200

    ajout-class

--- poo-User/index.php
+++ poo-User/index.php
@@ -1,6 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
 <?php 
    require_once('connexion-bdd.php');
    $connexion = new Database('localhost', 'user', 'root', '');
    $bdd = $connexion->PDOConnexion();
 
+
+   
 ?>
+
+
+<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form" action="traitement.php" method="post">
+
+					<span class="login100-form-title p-b-48">
+						<img src="images/logo.jpg" alt="Logo Agence Automobile Ardennaise" height="100px" style="top:3px;">
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
+						<input class="input100" type="text" name="email">
+						<span class="focus-input100" data-placeholder="Email"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<span class="btn-show-pass">
+							<i class="zmdi zmdi-eye"></i>
+						</span>
+						<input class="input100" type="password" name="pass">
+						<span class="focus-input100" data-placeholder="Password"></span>
+					</div>
+
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button class="login100-form-btn">
+								Login
+							</button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
