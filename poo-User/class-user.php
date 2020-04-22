53db84771e9777bbcf669d36b3cf39ad18fdb15f
Mat08251 <duguet@simplon-charleville.fr>
2020-04-22 11:56:12 +0200

    test-v12

--- poo-User/class-user.php
+++ poo-User/class-user.php
@@ -2,15 +2,13 @@
 
    class User
    {
-       private $_nomUser;
-       private $_prenomUser;
+       private $_pseudoUser;
        private $_passwordUser;
        private $_emailUser;
    }
 
    public function __construct( $nomUser ) {
-       $this->_nimUser = $_nomUser;
-       $this->_prenomUser = $_prenomUser;
+       $this->_nimUser = $_pseudoUser;
        $this->_passwordUser = $_passwordUser;
        $this->_emailUser = $_emailUser;
    }
--- poo-User/connexion-bdd.php
+++ poo-User/connexion-bdd.php
@@ -10,5 +10,28 @@ try {
     //en cas d'erreur, un message s'affiche et tout se stop
     die('Erreur : '.$e->getMessage());
 }
+class database {
+    //varaibles
+    protected $_host;
+    protected $_dbname;
+    protected $_username;
+    protected $_password;
 
+    //création du constructeur
+    public function __construct($_host, $_dbname, $_username, $_password) {
+        $this->_host = $_host;
+        $this->_dbname = $_dbname;
+        $this->_username = $_username;
+        $this->_password = $_password;
+    }
+
+    //fonction permettant de se connecter
+    public function PDOConnexion() {
+        $bdd = new PDO('mysql:host='.$this->_host.'; dbname='.$this->_dbname, $this->_username, $this->_password);
+        $bdd ->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $bdd ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //faire un return de la BDD ou non en fonction de la ou on utilise la connexion
+        return $bdd;
+    }
+}
 ?>
