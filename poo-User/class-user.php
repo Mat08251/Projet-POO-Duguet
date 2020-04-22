994d72244771de9b742579a6ca73c6addd5bab34
Mat08251 <duguet@simplon-charleville.fr>
2020-04-22 12:16:49 +0200

    class-user

--- poo-User/class-user.php
+++ poo-User/class-user.php
@@ -1,15 +1,38 @@
 <?php 
 
-   class User
-   {
-       private $_pseudoUser;
-       private $_passwordUser;
-       private $_emailUser;
-   }
-
+class User
+{
+    private $_pseudoUser;
+    private $_passwordUser;
+    private $_emailUser;
+   
    public function __construct( $nomUser ) {
        $this->_nimUser = $_pseudoUser;
        $this->_passwordUser = $_passwordUser;
        $this->_emailUser = $_emailUser;
    }
+
+   public function getpseudoUser(){
+    return $this->_pseudoUser;
+  }
+
+  public function getpasswordUser(){
+    return $this->_passwordUser;
+  }
+
+  public function getemailUser(){
+    return $this->_emailUser;
+  }
+
+
+  public function affiche(){
+      // REQUETE PERMETTANT LA RECHERCHE DES UTILISATEURS
+        $req = $bdd->prepare("SELECT * FROM user WHERE pseudoUser = :pseudoUser AND emailUser = :emailUser AND passwordUser = :passwordUser");
+        $req->execute(array(
+                    'pseudoUser' => $_POST["pseudoUser"],
+                    'emailUser' => $_POST["emailUser"],
+                    'passwordUser' => $_POST["passwordUser"]
+        ));
+  }
+
 ?>
